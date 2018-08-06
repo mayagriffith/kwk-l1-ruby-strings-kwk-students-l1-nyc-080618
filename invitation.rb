41b55ded8d5d0d699fb03537d453dcf8a2256af1
mayagriffith <mayaegriffith@gmail.com>
2018-08-06 19:38:38 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -10,10 +10,10 @@ time = gets.chomp
 puts "host name?"
 host_name = gets.chomp
 # Try starting out with puts'ing a string.
-Dear #{guest_name},
+puts "Dear #{guest_name},
  
 You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
  
 Sincerely,
  
-#{host_name}
+#{host_name}"
